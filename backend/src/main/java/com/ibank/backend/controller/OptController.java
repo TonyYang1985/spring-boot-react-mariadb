69019a76bdab680945ad984e53dc4e79e1224310
Tony <yangxiangjiang19880805@gmail.com>
2022-09-11 14:11:57 +0800

    up

--- backend/src/main/java/com/ibank/backend/controller/OptController.java
+++ backend/src/main/java/com/ibank/backend/controller/OptController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.ibank.backend.service.OptService;
+import com.ibank.backend.utils.PraseJwtTest;
 import com.ibank.backend.vo.request.TopUpResquest;
 import com.ibank.backend.vo.request.TranferRequest;
 import com.ibank.backend.vo.response.TopUpResponse;
 import com.ibank.backend.vo.response.TranferResponse;
 
+import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -29,7 +32,8 @@ public class OptController {
   @PostMapping(value="/topup")
   @ResponseBody
   public  TopUpResponse topup(@RequestBody TopUpResquest request,@RequestHeader("Authorization") String token) throws Exception {
-    log.info("topup save request {}",token);
+      //Claims cls =PraseJwtTest.tokenToOut(token.replace("Bearer ", ""));
+      //log.info("topup save request {}",cls.get("jti"));
       return  optService.topup(request);
      }   
      
--- backend/src/main/java/com/ibank/backend/utils/PraseJwtTest.java
+++ backend/src/main/java/com/ibank/backend/utils/PraseJwtTest.java
@@ -4,10 +4,11 @@ import io.jsonwebtoken.Jwts;
 
 
 public class PraseJwtTest {
-    public static void tokenToOut(String token) {
+    public static Claims tokenToOut(String token) {
         Claims claims = Jwts.parser()
                 .setSigningKey("my-123")
                 .parseClaimsJws(token)
                 .getBody();
+        return claims;
     }
 }
