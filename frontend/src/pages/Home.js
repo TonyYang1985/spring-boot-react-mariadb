0534996853c038f57471b62cc8096a80d279a499
Tony <yangxiangjiang19880805@gmail.com>
2022-09-11 12:47:18 +0800

    up

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,31 +1,37 @@
 import React, { useState, useEffect } from "react";
-
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 import UserService from "../services/user.service";
 import AuthService from "../services/auth.service";
+
 
 const Home = () => {
   const [content, setContent] = useState("");
   const currentUser = AuthService.getCurrentUser();
+  const [user, setUsers] = useState([]);
 
   useEffect(() => {
     UserService.getPublicContent().then(
       (response) => {
         setContent(JSON.stringify(response.data.users));
+        setUsers(response.data.users)
       },
       (error) => {
-
         setContent(error);
       }
     );
   }, []);
 
-  return (
-    <div className="container">
-      <header className="jumbotron">
-        <h3>{content}</h3>
-      </header>
-    </div>
-  );
+ 
+  return (<div>
+  <div className="card">
+      <DataTable value={user} responsiveLayout="scroll">
+          <Column field="id" header="ID"></Column>
+          <Column field="username" header="Name"></Column>
+          <Column field="email" header="Email"></Column>
+      </DataTable>
+  </div>
+</div>);
 };
 
 export default Home;
